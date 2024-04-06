ff00d78fda001070d9ebf0e4c5561ae82d938c59
OPISOF <vas6628346@icloud.com>
2024-04-06 09:54:27 +0300

    icon for site

--- js/main.js
+++ js/main.js
@@ -1,30 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let btn = document.querySelector('.btnMenu');
+    let menu = document.querySelector('.menu');
 
-
-$(document).ready(function() {
-    $('.btn').click(function() {
-        $('.message').text('Button clicked!');
+    btn.addEventListener('click', function() {
+        btn.classList.toggle('active');
+        btn.classList.toggle('not-active');
+        dropDown(menu);
     });
+
+    function dropDown(element) {
+        if (getComputedStyle(element).display === 'none') {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        }
+    }
 });
 
-var btn = $('.btn');
-btn.on('click', function() {
-  $(this).toggleClass('active not-active');
-});
 
-//let menu = $('menu');
-//
-//function dropDown(element){
-//    if(element.style.display === 'none'){
-//        element.style.display = 'block';
-//    }
-//    else{
-//        element.style.display = 'none';
-//    }
-//};
 
-//btn.on('click', function(){
-//    dropDown(menu);
-//});
+
 
 
 
